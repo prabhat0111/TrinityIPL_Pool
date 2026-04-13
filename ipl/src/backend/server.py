3ec744b380810ab317c82914d5fe76e92d4ae0fa
DigvijayDutt <duttdigvijay@gmail.com>
2026-04-13 13:44:54 +0530

    minor

--- ipl/src/backend/server.py
+++ ipl/src/backend/server.py
@@ -30,7 +30,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://ipl.energeticitsolutions.com"
+        "https://ipl.energeticitsolutions.com",
+         "http://localhost:5173"
     ],
     allow_credentials=True,
     allow_methods=["*"],
